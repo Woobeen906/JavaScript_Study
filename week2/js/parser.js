33f3c8e140cdb7c7f11756e85850c928cef1cab0
Woobeen906 <67691520+Woobeen906@users.noreply.github.com>
2021-03-12 14:24:24 +0900

    2주차 과제 업로드

---
+++ week2/js/parser.js
@@ -0,0 +1,6 @@
+// let age=prompt("나이를 입력하시오","0");
+// console.log("입력한 나이" + age + "보다 10살 많은 " +(Number(age)+10) + "입니다.");
+
+let num1=prompt("정수를 입락하세요","0");
+let num2=prompt("실수를 입력하세요","0");
+console.log((Number(num1)+Number(num2)).toFixed(2));
