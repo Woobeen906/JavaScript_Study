c9e1b28d8892c11fc288828d26e4f583a947d9ab
Woobeen906 <67691520+Woobeen906@users.noreply.github.com>
2021-04-07 21:21:46 +0900

    5주차 객체 배열

--- week5/js/week5_inclass.js
+++ week5/js/week5_inclass.js
@@ -1,14 +1,23 @@
-let product=[
-    {name:'바나나',price:1200},
-    {name:'사과',price:2000}
-];
+// let product=[
+//     {name:'바나나',price:1200},
+//     {name:'사과',price:2000}
+// ];
 
-product.push({name:'배',price:3000});
+// product.push({name:'배',price:3000});
 
-let sum = 0;
+// let sum = 0;
 
-for (let i in product) {
-    console.log(product[i].name);
-    sum += product[i].price;
-}
-console.log(sum);
+// for (let i in product) {
+//     console.log(product[i].name);
+//     sum += product[i].price;
+// }
+// console.log(sum);
+
+let ob={
+    name:"바나나",
+    price:1200,
+    print:function(){
+        console.log(this.name+" "+this.price);
+    }
+};
+ob.print();
