c2de7885987e400e761e6a905fefcd53a4ca9978
Woobeen906 <siugan@naver.com>
2021-04-16 23:38:59 +0900

    Update week5_inclass.js

--- week5/js/week5_inclass.js
+++ week5/js/week5_inclass.js
@@ -1,17 +1,4 @@
-// let product=[
-//     {name:'바나나',price:1200},
-//     {name:'사과',price:2000}
-// ];
 
-// product.push({name:'배',price:3000});
-
-// let sum = 0;
-
-// for (let i in product) {
-//     console.log(product[i].name);
-//     sum += product[i].price;
-// }
-// console.log(sum);
 
 let ob={
     name:"바나나",
